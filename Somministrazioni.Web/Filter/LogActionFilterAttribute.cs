9368ecd16e68cbad3923f7d4dccbcfc31892f751
fabriziodifonzo <spammail27mao@gmail.com>
2019-06-06 15:26:51 +0200

    Mfdifica per visualizzazione parametri.

--- Somministrazioni.Web/Filter/LogActionFilterAttribute.cs
+++ Somministrazioni.Web/Filter/LogActionFilterAttribute.cs
@@ -3,6 +3,7 @@ using Somministrazioni.Common.Constants;
 using Somministrazioni.Web.Constants;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -22,24 +23,50 @@ namespace Somministrazioni.Web.Filter
         {
             var methodType = filterContext.HttpContext.Request.HttpMethod;
             var name = $"{filterContext.ActionDescriptor.ControllerDescriptor.ControllerType}.{filterContext.ActionDescriptor.ControllerDescriptor.ControllerName}.{filterContext.ActionDescriptor.ActionName}";
-            var args = filterContext.ActionDescriptor.GetParameters().ToArray();
+            var args = ParameterHelper.PrintParameters(filterContext.ActionParameters);
+
 
             _log.Info($"Calling: {name} {methodType}");
             _log.Info($"Args: {args}");
 
+            watch = new Stopwatch();
+            watch.Start();
             base.OnActionExecuting(filterContext);
         }
 
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
+            base.OnActionExecuted(filterContext);
+
+            watch.Stop();
             var methodType = filterContext.HttpContext.Request.HttpMethod;
             var name = $"{filterContext.ActionDescriptor.ControllerDescriptor.ControllerType}.{filterContext.ActionDescriptor.ControllerDescriptor.ControllerName}.{filterContext.ActionDescriptor.ActionName}";
+            var executionTime = watch.ElapsedMilliseconds;
 
             _log.Info(($"Done ({name}) {methodType}"));
-
-            base.OnActionExecuted(filterContext);
+            _log.Info($"Execution Time: {executionTime} ms.");
         }
 
+        Stopwatch watch;
         readonly ILog _log;
+
+        static class ParameterHelper
+        {
+            public static string PrintParameters(IDictionary<string, object> mapParameters)
+            {
+                var paramBuilder = new StringBuilder();
+                foreach (var paramDescriptor in mapParameters)
+                {
+                    paramBuilder.Append(paramDescriptor.Value);
+                    paramBuilder.Append(",");
+                }
+                if (paramBuilder.Length > 0)
+                {
+                    paramBuilder.Remove(paramBuilder.Length - 1, 1);
+                }
+                
+                return paramBuilder.ToString();
+            }
+        }
     }
 }
