c38248f3e8a48f599f922d89d70da2ca4263cc57
fabriziodifonzo <spammail27mao@gmail.com>
2019-06-11 11:45:49 +0200

    Introduzione pagina di logout

---
+++ Somministrazioni.Web/Controllers/LogoutController.cs
@@ -0,0 +1,20 @@
+﻿using Somministrazioni.Web.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Somministrazioni.Web.Controllers
+{
+    public class LogoutController : Controller
+    {
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session[WebConstants.SESSIONNAME_IDOPERATORE] = null;
+
+            return View();
+        }
+    }
+}
